991ec4cf390ff09593f08e595d238a22ce0bd82f
jdw32 <jdw32@LAPTOP-4CA99QCP>
2019-09-30 20:25:06 +0900

    190930

--- splace/src/main/webapp/resources/js/spaceDetail.js
+++ splace/src/main/webapp/resources/js/spaceDetail.js
@@ -1,5 +1,8 @@
-// 글자수 세기 
-$(function() {
+$(document).ready(function(){
+	
+	
+
+	// 글자수 세기 
     $('.content').keyup(function (e){
         var content = $(this).val();
         $(this).height(((content.split('\n').length + 1) * 1.5) + 'em');
@@ -11,15 +14,19 @@ $(function() {
     	$(".content").val("");
     	$('.counter').html('0/300');
     });
-});
-
-//별
-$(document).ready(function(){
+    
+    //별
     $('#star_grade a').click(function(){
-        $(this).parent().children("a").removeClass("on");  /* 별점의 on 클래스 전부 제거 */ 
-        $(this).addClass("on").prevAll("a").addClass("on"); /* 클릭한 별과, 그 앞 까지 별점에 on 클래스 추가 */
-        return false;
+    	$(this).parent().children("a").removeClass("on");  /* 별점의 on 클래스 전부 제거 */ 
+    	$(this).addClass("on").prevAll("a").addClass("on"); /* 클릭한 별과, 그 앞 까지 별점에 on 클래스 추가 */
+    	return false;
     });
+    
+    $(".cancel").on("click",function(){
+    	$('#star_grade a').parent().children("a").removeClass("on");
+    });
+    
+	
 });
 
 
