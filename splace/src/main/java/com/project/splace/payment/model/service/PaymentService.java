5b22e21b8bb986a40ed04fa4f0ae5749cd411057
user1 <user1@user1-PC>
2019-10-06 17:58:01 +0900

    김샛별191006-1

--- splace/src/main/java/com/project/splace/payment/model/service/PaymentService.java
+++ splace/src/main/java/com/project/splace/payment/model/service/PaymentService.java
@@ -1,5 +1,13 @@
 package com.project.splace.payment.model.service;
 
+import com.project.splace.payment.model.vo.Payment;
+
 public interface PaymentService {
+
+	// 결제
+	public abstract int insertPayment(Payment payment);
+
+	// 결제 취소
+	public abstract int updatePaymentCancel(int bookId);
 	
 }
